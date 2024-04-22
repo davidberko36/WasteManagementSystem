2747be4316c4c7ee2f0d66ca81d97dcc8ffc533b
davidberko36 <130989471+davidberko36@users.noreply.github.com>
2024-04-22 04:31:18 +0000

    Added driver and vehicle

--- WasteManagementSystem/core/admin.py
+++ WasteManagementSystem/core/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Customer, Schedule
+from .models import Customer, Driver, Schedule, Issue, Collection
 
 # Register your models here.
 admin.site.register(Customer)
+admin.site.register(Driver)
 admin.site.register(Schedule)
+admin.site.register(Issue)
+admin.site.register(Collection)
