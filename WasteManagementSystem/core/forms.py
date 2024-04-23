5e37abe342133137bbeffa8f82f332ec51396b8f
davidberko36 <130989471+davidberko36@users.noreply.github.com>
2024-04-23 15:32:34 +0000

    Corrected models, serializers and forms

--- WasteManagementSystem/core/forms.py
+++ WasteManagementSystem/core/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from .models import Customer
+from .models import Customer, Driver
 
 
 class SignupForm(UserCreationForm):
@@ -22,3 +22,21 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = Customer
         fields = ['other_names', 'last_name', 'address', 'email', 'mobile_phone', 'username', 'date_of_birth', 'customer_type']
+
+
+
+
+
+class DriverCreationForm(UserCreationForm):
+    other_names=forms.CharField(max_length=60, required=True)
+    last_name=forms.CharField(max_length=60, required=True)
+    email=forms.EmailField(max_length=255, required=True)
+    mobile_phone=forms.CharField(max_length=14, required=True)
+    date_of_birth=forms.DateField(required=True)
+    driver_license_number=forms.CharField(max_length=30, required=True)
+
+
+
+    class Meta:
+        model = Driver
+        fields = ['other_names', 'last_name', 'email', 'mobile_phone', 'date_of_birth', 'driver_license_number']
