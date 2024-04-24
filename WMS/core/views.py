35c77e42ebb42dd977e268f48cc6ffc0b70dcaed
davidberko36 <130989471+davidberko36@users.noreply.github.com>
2024-04-24 14:46:10 +0000

    Added some views

--- WMS/core/views.py
+++ WMS/core/views.py
@@ -1,4 +1,29 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.contrib.auth import login
+from .forms import SignupForm, DriverCreationForm
 
-# Create your views here.
+def customer_signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return JsonResponse({'success': True, 'message': 'Customer registration successful'})
+        else:
+            return JsonResponse({'success': False, 'errors': form.errors})
+    else:
+        form = SignupForm()
+    return JsonResponse({'success': False, 'message': 'Invalid request method'})
 
+def driver_signup(request):
+    if request.method == 'POST':
+        form = DriverCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return JsonResponse({'success': True, 'message': 'Driver registration successful'})
+        else:
+            return JsonResponse({'success': False, 'errors': form.errors})
+    else:
+        form = DriverCreationForm()
+    return JsonResponse({'success': False, 'message': 'Invalid request method'})
