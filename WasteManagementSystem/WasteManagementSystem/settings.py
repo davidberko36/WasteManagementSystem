9dd592e6aa661124f9bfa75f95e35adf6ad55226
davidberko36 <130989471+davidberko36@users.noreply.github.com>
2024-04-24 13:02:57 +0000

    Changed database to sqlite3 for now

--- WasteManagementSystem/WasteManagementSystem/settings.py
+++ WasteManagementSystem/WasteManagementSystem/settings.py
@@ -76,13 +76,8 @@ WSGI_APPLICATION = 'WasteManagementSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'waste_management_system',
-        'USER': 'root',
-        'PASSWORD': 'sylphlike',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
